63c0d359cab5a929f7304e5a5f5236f6aa0d3ef1
FranRguez99 <FranRguez99@gmail.com>
2023-01-17 17:17:34 +0100

    Actualización de la interfaz: Ajustados botones, menús y formulario de usuarios y libros

--- src/main/java/safa/ad_biblioteca/controller/ControladorPrincipal.java
+++ src/main/java/safa/ad_biblioteca/controller/ControladorPrincipal.java
@@ -1,25 +1,20 @@
 package safa.ad_biblioteca.controller;
 
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 import safa.ad_biblioteca.model.Conexion;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ControladorPrincipal implements Initializable {
-    @FXML
-    private Label welcomeText;
+
+    // Elementos JavaFX
+
 
     // Atributos
     Conexion conexion = new Conexion();
 
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
