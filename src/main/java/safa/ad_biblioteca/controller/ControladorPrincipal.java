6eca0b9c1c4ac32c96d70335d68d05dd5d691b9e
FranRguez99 <FranRguez99@gmail.com>
2023-01-18 12:36:39 +0100

    Interfaz completada

--- src/main/java/safa/ad_biblioteca/controller/ControladorPrincipal.java
+++ src/main/java/safa/ad_biblioteca/controller/ControladorPrincipal.java
@@ -14,55 +14,124 @@ public class ControladorPrincipal implements Initializable {
 
     // Elementos JavaFX
     @FXML
-    private Button btnLibrosDevoluciones;
-
-    @FXML
-    private Button btnLibrosPrestamos;
-
-    @FXML
-    private Button btnLibrosPrincipal;
-
-    @FXML
-    private Button btnLibrosUsuarios;
-
-    @FXML
-    private Button btnPrincipalDevoluciones;
-
-    @FXML
     private Button btnPrincipalLibros;
-
-    @FXML
-    private Button btnPrincipalPrestamos;
-
-    @FXML
-    private Button btnPrincipalUsuarios;
-
-    @FXML
-    private Button btnUsuariosDevoluciones;
-
-    @FXML
-    private Button btnUsuariosLibros;
-
-    @FXML
-    private Button btnUsuariosPrestamos;
-
-    @FXML
-    private Button btnUsuariosPrincipal;
-
-    @FXML
-    private Pane panelLibros;
-
-    @FXML
-    private Pane panelPrincipal;
 
     @FXML
     private Pane panelRegistroLibros;
 
     @FXML
+    private Pane panelUsuarios;
+
+    @FXML
+    private Button btnRegLibVolver;
+
+    @FXML
+    private Button btnPrestamosPrestar;
+
+    @FXML
+    private Button btnPrincipalUsuarios;
+
+    @FXML
+    private Button btnUsuariosPrincipal;
+
+    @FXML
+    private Button btnDevolLibros;
+
+    @FXML
+    private Button btnPrestamosDevol;
+
+    @FXML
+    private Button btnUsuariosModif;
+
+    @FXML
+    private Pane panelDevol;
+
+    @FXML
+    private Button btnPrincipalDevoluciones;
+
+    @FXML
+    private Button btnPrincipalPrestamos;
+
+    @FXML
+    private Button btnLibrosModificar;
+
+    @FXML
+    private Button btnUsuariosBorrar;
+
+    @FXML
+    private Button btnRegLibREgistrar;
+
+    @FXML
+    private Button btnLibrosUsuarios;
+
+    @FXML
     private Pane panelRegistroUsuarios;
 
     @FXML
-    private Pane panelUsuarios;
+    private Button btn;
+
+    @FXML
+    private Button btnLibrosNuevo;
+
+    @FXML
+    private Button btnPrestamosUsuarios;
+
+    @FXML
+    private Button btnDevolPrincipal;
+
+    @FXML
+    private Button btnDevolDevolver;
+
+    @FXML
+    private Pane panelPrincipal;
+
+    @FXML
+    private Button btnDevolUsuarios;
+
+    @FXML
+    private Button btnLibrosBorrar;
+
+    @FXML
+    private Button btnUsuariosLibros;
+
+    @FXML
+    private Button btnUsuariosNuevo;
+
+    @FXML
+    private Button btnDevolPrestamos;
+
+    @FXML
+    private Button btnUsuariosPrestamos;
+
+    @FXML
+    private Button btnUsuariosBuscar;
+
+    @FXML
+    private Button btnUsuariosPrincipal1;
+
+    @FXML
+    private Button btnRegUsuarioVolver;
+
+    @FXML
+    private Pane panelLibros;
+
+    @FXML
+    private Button btnLibrosPrincipal;
+
+    @FXML
+    private Button btnRegUsuarioRegistrar;
+
+    @FXML
+    private Pane panelPrestamos;
+
+    @FXML
+    private Button btnLibrosPrestamos;
+
+    @FXML
+    private Button btnLibrosDevoluciones;
+
+    @FXML
+    private Button btnUsuariosDevoluciones;
 
     // Atributos
     Conexion conexion = new Conexion();
